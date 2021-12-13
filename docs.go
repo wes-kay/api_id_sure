8975fc83986d252ef0b29a68eed01275fa44738c
trep t <keeling.wesley@gmail.com>
2021-12-13 07:56:14 -0800

    updating path

--- docs.go
+++ docs.go
@@ -90,7 +90,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "api.idsure.io",
+	Host:        "",
 	BasePath:    "/v1",
 	Schemes:     []string{},
 	Title:       "Digital ID",
